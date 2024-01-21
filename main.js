b5e888b8210cffb81949968ef35c6fbd13c5e3b3
ericom1027 <146744741+ericom1027@users.noreply.github.com>
2024-01-21 18:34:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,112 +1,95 @@
-const menuBtn = document.querySelector('.menu-btn')
-const navlinks = document.querySelector('.nav-links')
+const wordsSet1 = ['Powered by: EMD IT Solutions.'];
 
-menuBtn.addEventListener('click',()=>{
-navlinks.classList.toggle('mobile-menu')
-})
+let part1;
+let i = 0;
+let offset1 = 0;
+const len1 = wordsSet1.length;
+let forwards1 = true;
+let skip_count1 = 0;
+const skip_delay = 15;
+const speed = 70;
 
+const wordflick = function (wordsSet, elementSelector) {
+  let len = wordsSet.length;
+  let i = 0;
+  let offset = 0;
+  let forwards = true;
+  let skip_count = 0;
 
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
-
-function showSlides() {
-  slides.forEach((slide) => slide.style.display = 'none');
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1; }
-  slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 2000); // Change image every 2 seconds (2000ms)
-}
-
-showSlides(); // Start the slideshow
-
-
-// Send message to gmail account//
-const form = document.querySelector("form");
-const firstname= document.getElementById("Firstname");
-const Lastname= document.getElementById("Lastname");
-const email= document.getElementById("Email");
-const Phone= document.getElementById("phone");
-const msg= document.getElementById("Message");
-
-function sendEmail() {
-const bodyMessage = `Firstname: ${firstname.value}<br> Lastname: ${Lastname.value}<br> Email: ${email.value}<br> Mobile no: ${Phone.value}<br> Message: ${msg.value}`;
-
-  Email.send({
-    SecureToken : "c595ecb9-e610-48f0-baf1-cff3e163e767",
-    To : 'greatwavesmabical@gmail.com',
-    From : "greatwavesmabical@gmail.com",
-    Subject : "Message from Sender",
-    Body : bodyMessage
-}).then(
-  message => {
-    if (message == "OK") {
-      Swal.fire({
-        title: "Success!",
-        text: "Message sent successfully!",
-        icon: "success"
-      });
-    }
-
-  }
-);
-}
-
-form.addEventListener("submit", (e)=> {
-  e.preventDefault();
-  
-  sendEmail();
-
-});
- // Clear text filed //
-document.getElementById("myForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevents the default form submission
-
-  // Resetting the form fields after submission
-  document.getElementById("myForm").reset();
-});
-
-var wordsSet1 = ['Powered by: EMD IT Solutions'];
-
-var part1;
-var i = 0;
-var offset1 = 0;
-var len1 = wordsSet1.length;
-var forwards1 = true;
-var skip_count1 = 0;
-var skip_delay = 15;
-var speed = 70;
-
-var wordflick = function () {
   setInterval(function () {
-    if (forwards1) {
-      if (offset1 >= wordsSet1[i].length) {
-        ++skip_count1;
-        if (skip_count1 === skip_delay) {
-          forwards1 = false;
-          skip_count1 = 0;
+    if (forwards) {
+      if (offset >= wordsSet[i].length) {
+        ++skip_count;
+        if (skip_count === skip_delay) {
+          forwards = false;
+          skip_count = 0;
         }
       }
     } else {
-      if (offset1 === 0) {
-        forwards1 = true;
-        offset1 = 0;
+      if (offset === 0) {
+        forwards = true;
       }
     }
 
-    part1 = wordsSet1[i].substr(0, offset1);
+    part1 = wordsSet[i].substr(0, offset);
 
-    if (skip_count1 === 0) {
-      if (forwards1) {
-        offset1++;
+    if (skip_count === 0) {
+      if (forwards) {
+        offset++;
       } else {
-        offset1--;
+        offset--;
       }
     }
 
-    $('.words1').text(part1);
+    $(elementSelector).text(part1);
   }, speed);
 };
 
 $(document).ready(function () {
-  wordflick();
+  wordflick(wordsSet1, '.words1');
 });
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("FullName");
+const email = document.getElementById("Email");
+const concern = document.getElementById("concern");
+const contact = document.getElementById("contactno");
+const message = document.getElementById("Message");
+
+function sendEmail() {
+  const bodyMessage = `fullname: ${fullName.value}<br> email: ${email.value}<br> contact: ${contact.value}<br> concern: ${concern.value}<br>message: ${message.value}`;
+
+  Email.send({
+    SecureToken: "c595ecb9-e610-48f0-baf1-cff3e163e767",
+    To: 'greatwavesmabical@gmail.com',
+    From: "greatwavesmabical@gmail.com",
+    Subject: "Message from Sender",
+    Body: bodyMessage
+  }).then(
+    message => {
+      if (message === "OK") {
+        Swal.fire({
+          title: "Success!",
+          text: "Message sent successfully!",
+          icon: "success"
+        });
+      }
+    }
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
+
+document.getElementById("form-contact").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  
+
+  document.getElementById("form-contact").reset();
+
+ 
+});
+
